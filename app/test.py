2fe6cc38ad8e6c45becc74acc472d9e0067f126e
SH1268 <115600478+SH1268@users.noreply.github.com>
2022-11-30 19:42:57 -0500

    Update test.py

--- app/test.py
+++ app/test.py
@@ -2,7 +2,7 @@ import requests
 
 url = "https://api-nba-v1.p.rapidapi.com/games"
 
-querystring = {"team":"3"}
+querystring = {"season":"2022", "team":"15", "date":"2022-11-10"}
 
 headers = {
 	"X-RapidAPI-Key": "d7f5a3d00dmsh6e9d7042e9a3dccp163db9jsn59e8d3fd2b59",
